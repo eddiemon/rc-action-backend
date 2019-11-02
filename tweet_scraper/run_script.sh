d8266c1cbfcb1fa55b3636ce68c37c08c45835dc
croaton <dmytro.shervarly@gmail.com>
2019-11-02 21:10:30 +0100

    Improved twitter query

--- tweet_scraper/run_script.sh
+++ tweet_scraper/run_script.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scrapy crawl TweetScraper -a query="danger since:2019-10-25 until:2019-11-02 near:Stockholm"
+scrapy crawl TweetScraper -a query="danger OR terrorist OR emergency OR disaster OR conflict since:2019-10-25 until:2019-11-03 near:Stockholm"

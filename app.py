da239ea2d1ad15b085c660d5c8789769549a7a2b
Eddie Sundvall <eddie.sundvall@gmail.com>
2019-11-02 17:29:36 +0100

    Set port to 8080

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def news():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=80)
+    app.run(threaded=True, port=8080)

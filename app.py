9a85a9b3c6038c3a47ec25d14b01394c043e62d4
croaton <dmytro.shervarly@gmail.com>
2019-11-02 18:21:39 +0100

    One more try

--- app.py
+++ app.py
@@ -86,4 +86,8 @@ def favicon():
                                'static/favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    PORT = 8000
+    if os.getenv("PORT") is not None:
+        PORT = os.getenv("PORT")
+    app.run(threaded=True, port=PORT)
+    app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
